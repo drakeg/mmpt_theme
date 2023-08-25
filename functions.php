53c406f0a50cfc421efb424b9d922a69ffbdaba6
Greg Drake <greg@madmallards.com>
2023-08-25 19:58:26 -0400

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,4 +1,16 @@
 <?php
+function your_theme_setup() {
+    // Add theme support for custom logo.
+    add_theme_support('custom-logo', array(
+        'height'      => 100,   // set the max height in pixels
+        'width'       => 400,   // set the max width in pixels
+        'flex-height' => true,  // enable flexible height
+        'flex-width'  => true,  // enable flexible width
+        'header-text' => array('site-title', 'site-description'),
+    ));
+}
+add_action('after_setup_theme', 'your_theme_setup');
+
 function theme_enqueue_styles() {
     wp_enqueue_style('main-style', get_stylesheet_uri());
 }
