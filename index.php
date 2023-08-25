3bb9d05b601532df228bb5d7c8312f8071d912cd
Greg Drake <greg@madmallards.com>
2023-08-25 19:43:17 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,16 +1,19 @@
 <?php get_header(); ?>
 
-<?php if(have_posts()): ?>
-    <?php while(have_posts()): the_post(); ?>
-        <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-        <div><?php the_excerpt(); ?></div>
-    <?php endwhile; ?>
-    
-    <div class="pagination">
-        <?php previous_posts_link('&laquo; Newer Posts') ?>
-        <?php next_posts_link('Older Posts &raquo;') ?>
-    </div>
-    
-<?php endif; ?>
+<main class="blog-container">
+    <?php if(have_posts()): ?>
+        <?php while(have_posts()): the_post(); ?>
+            <article class="blog-snippet">
+                <h2 class="post-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+                <div class="post-excerpt"><?php the_excerpt(); ?></div>
+                <a href="<?php the_permalink(); ?>" class="read-more">Read More</a>
+            </article>
+        <?php endwhile; ?>
+
+        <div class="pagination">
+            <?php wp_pagenavi(); ?>
+        </div>
+    <?php endif; ?>
+</main>
 
 <?php get_footer(); ?>
