30d31dc90367032f1adc50102e43f538953e0506
Greg Drake <greg@madmallards.com>
2023-10-29 18:44:13 -0400

    Update footer.php
    Added bloglovin link.

--- footer.php
+++ footer.php
@@ -1,5 +1,6 @@
     <footer>
         <p>&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?></p>
+        <a class="blsdk-follow" href="https://www.bloglovin.com/blogs/mad-mallard-personal-training-21523413" target="_blank" data-blsdk-type="button">Follow</a><script>(function(d, s, id) {var js, fjs = d.getElementsByTagName(s)[0];if (d.getElementById(id)) return;js = d.createElement(s);js.id = id;js.src = "https://www.bloglovin.com/widget/js/loader.js?v=1";fjs.parentNode.insertBefore(js, fjs);}(document, "script", "bloglovin-sdk"))</script>
     </footer>
     <?php wp_footer(); ?>
 </body>
