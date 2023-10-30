975cb2bf92f527537a19ed1484cb3f9e87d40892
Greg Drake <greg@madmallards.com>
2023-10-29 22:55:58 -0400

    Navbar

--- searchform.php
+++ searchform.php
@@ -1,5 +1,4 @@
-<form class="d-flex" role="search" method="get" action="<?php echo home_url( '/' ); ?>>
-    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" name="d" title="Search for:">
-    <button class="btn btn-outline-success" type="submit" value=
-    "Search">Search</button>
+<form class="d-flex" role="search" method="get" action="<?php echo home_url( '/' ); ?>">
+    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" name="s" title="Search for:">
+    <button class="btn btn-outline-success" type="submit" value="Search">Search</button>
 </form>
