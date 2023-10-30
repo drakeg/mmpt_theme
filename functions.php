a7d0f90ad4e70570a0baefcc65cb275aa22df412
Greg Drake <greg@madmallards.com>
2023-10-30 00:04:34 -0400

    Change enqueues

--- functions.php
+++ functions.php
@@ -61,10 +61,13 @@ add_action('wp_enqueue_scripts', 'theme_enqueue_scripts');
  * Enqueue scripts and styles for the front end.
  */
 function theme_enqueue_styles() {
-    // Enqueue your main stylesheet
-    wp_enqueue_style('main-style', get_stylesheet_uri());
+    // Enqueue bootstrap and fontawesome
     wp_enqueue_style('bootstrap-css', 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css');
     wp_enqueue_style('fontawesome-css', 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css');
+
+    // Enqueue your main stylesheet
+    wp_enqueue_style('main-style', get_stylesheet_uri());
+    
     // Get logo URL
     $custom_logo_id = get_theme_mod('custom_logo');
     $logo = wp_get_attachment_image_src($custom_logo_id , 'full');
